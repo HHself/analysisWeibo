6a1d25662b085146c2ec3d2e4482e195aa28915b
huhao <hh87know@gmail.com>
2015-11-10 15:27:06 +0800

    Signed-off-by: huhao <hh87know@gmail.com>

--- functiontools.py
+++ functiontools.py
@@ -184,26 +184,58 @@ def tongji_userfre():
     temp_userfre = sorted(data_user.iteritems(), key = lambda x:x[1], reverse = True)[:50]
     writefile({i[0]:i[1] for i in temp_userfre}, output2 + "data_userfre.txt")
 
+# def cutwords_stars(sentence): 
+# 	stars = [s.decode("utf-8") for s in file("stars.txt")]
+# 	stopwords = [s.replace("\n", "").decode("utf-8") for s in file("stopwords.txt")]
+# 	cutcontent = [w for w in list(jieba.cut(sentence, cut_all = False)) if w not in stopwords and w in stars]
+# 	return cutcontent
 def cutwords_stars(sentence): 
-	stars = [s.decode("utf-8") for s in file("stars.txt")]
-	stopwords = [s.replace("\n", "").decode("utf-8") for s in file("stopwords.txt")]
-	cutcontent = [w for w in list(jieba.cut(sentence, cut_all = False)) if w not in stopwords and w in stars]
-	return cutcontent
+  stars = [s.decode("utf-8") for s in file("stars.txt")]
+  cutcontent = [w for w in stars if w in sentence]
+  return cutcontent
 
 def cutwords_keywords(sentence):
 	s = SnowNLP(sentence.decode("utf-8"))
-	s.cut_keywords()
+	return s.cut_keywords(3)
 
 def cutwords_sentiment(sentence): 
 	s = SnowNLP(sentence.decode("utf-8"))
-	senti = s.sentiment()>0.5 and 1 or 0
-
+	senti = s.sentiment()
+    st = 0
+    if senti > 0.6:
+        st = 1
+    elif senti >= 0.4 and senti<=0.6:
+        st = 0
+    else:
+        st = -1
+    return st
 
 def cutwords_area(sentence): 
     district = [u"北京", u"天津", u"重庆", u"上海", u"河北", u"山西", u"辽宁", u"吉林", u"黑龙江", u"江苏", u"浙江", u"安徽", u"福建", u"江西", u"山东", u"河南", u"湖北", u"湖南", u"广东", u"海南", u"四川", u"贵州", u"云南", u"陕西", u"甘肃", u"青海", u"台湾"]
     words = pseg.cut(sentence)
     district_word = [w.word for w in words if str(w.flag) == 'ns']
     return district_word
+
+def cutwords_phone(sentence): 
+   phones = [u"三星", u"小米", u"苹果", u"华为", u"诺基亚", u"联想", u"索尼", u"魅族", u"酷派", u"金立"]
+   cutcontent = [w for w in stars if w in sentence]
+   return cutcontent
+
+def cutwords_internet(sentence): 
+   internet = [u"百度", u"阿里", u"腾讯", u"京东", u"网易", u"搜狐", u"新浪", u"携程", u"优酷"]
+   cutcontent = [w for w in stars if w in sentence]
+   return cutcontent
+
+def cutwords_social(sentence): 
+   social = [u"微博", u"知乎", u"贴吧", u"微信", u"QQ", u"糗事百科", u"人人", u"豆瓣", u"世纪佳缘"]
+   cutcontent = [w for w in stars if w in sentence]
+   return cutcontent
+
+def cutwords_sentiword(sentence): 
+   sentiword = [u"爱", u"恨", u"开心", u"伤心", u"哈哈", u"唉", u"么么哒", u"萌萌哒"]
+   cutcontent = [w for w in stars if w in sentence]
+   return cutcontent
+
 
 def tongji_msg():
     precol = ["userID", "username", "screenname", "msginfo", "source", "forwardNum", "commentNum", "releasetime", "etuser"]
@@ -212,6 +244,10 @@ def tongji_msg():
     data_keywords = {}
     data_sentiment = {}
     data_area = {}
+    data_phone = {}
+    data_internet = {}
+    data_social = {}
+    data_sentiword = {}
     for line in file(filepath2 + "content.txt"):
         #print line
         num +=1
@@ -221,32 +257,68 @@ def tongji_msg():
         d = pd.read_csv(filepath2 + line.replace("\n",""))
         d = d.drop(["userID", "username", "screenname", "source", "forwardNum", "commentNum", "releasetime"], axis = 1)
         d["cut_stars"] = d['msginfo'].apply(cutwords_stars)
-        # d["cut_keywords"] = d['msginfo'].apply(cutwords_keywords)
-        # d["cut_sentiment"] = d['msginfo'].apply(cutwords_sentiment)
+        d["cut_keywords"] = d['msginfo'].apply(cutwords_keywords)
+        d["cut_sentiment"] = d['msginfo'].apply(cutwords_sentiment)
         d["cut_area"] = d['msginfo'].apply(cutwords_area)
+        d["cut_phone"] = d['msginfo'].apply(cutwords_phone)
+        d["cut_internet"] = d['msginfo'].apply(cutwords_internet)
+        d["cut_social"] = d['msginfo'].apply(cutwords_social)
+        d["cut_sentiword"] = d['msginfo'].apply(cutwords_sentiword)
         for ind in d.index:
             sta = d["cut_stars"][ind]
-        	# keyw = d["cut_keywords"][ind]
-        	# senti = d["cut_sentiment"][ind]
-            ar = d["cut_area"][ind]
+        	ar = d["cut_area"][ind]
+        	senti = d["cut_sentiment"][ind]
+            keyw = d["cut_keywords"][ind]
+            ph = d["cut_phone"][ind]
+            inte = d["cut_internet"][ind]
+            so = d["cut_social"][ind]
+            sentiw = d["cut_sentiword"][ind]
+
             for s in sta:
         	    data_stars.setdefault(s, 0)
         	    data_stars[s] += 1
             for a in ar:
                 data_area.setdefault(ar, 0)
                 data_area[a] += 1 
-            # data_keywords.setdefault(keyw, 0)
-            # data_sentiment.setdefault(senti, 0)
-            # data_keywords[keyw] += 1
-            # data_sentiment[senti] += 1
-                
+            for se in senti:
+                data_sentiment.setdefault(se, 0)
+                data_sentiment[se] += 1 
+            for k in keyw:                
+                data_keywords.setdefault(k, 0)
+                data_keywords[keyw] += 1
+            for p in ph:
+                data_phone.setdefault(p, 0)
+                data_phone[p] += 1
+            for i in inte:
+                data_internet.setdefault(i, 0)
+                data_internet[i] += 1
+            for s in so:
+                data_social.setdefault(s, 0)
+                data_social[s] += 1
+            for sen in sentiw:
+                data_sentiword.setdefault(sen, 0)
+                data_sentiword[sen] += 1 
+     
         #except:
         #	print line
 
     data_stars  = sorted(data_stars.iteritems(), key = lambda x:x[1], reverse = True)[:50]
     data_area = sorted(data_area.iteritems(), key = lambda x:x[1], reverse = True)[:50]
+    data_sentiment  = sorted(data_sentiment.iteritems(), key = lambda x:x[1], reverse = True)[:50]
+    data_keywords = sorted(data_keywords.iteritems(), key = lambda x:x[1], reverse = True)[:50]
+    data_phone  = sorted(data_phone.iteritems(), key = lambda x:x[1], reverse = True)[:50]
+    data_internet = sorted(data_internet.iteritems(), key = lambda x:x[1], reverse = True)[:50]
+    data_social  = sorted(data_social.iteritems(), key = lambda x:x[1], reverse = True)[:50]
+    data_sentiword = sorted(data_sentiword.iteritems(), key = lambda x:x[1], reverse = True)[:50]
+    
     writefile({i[0]:i[1] for i in data_stars}, output2 + "data_stars.txt")
     writefile({i[0]:i[1] for i in data_area}, output2 + "data_area.txt")
+    writefile({i[0]:i[1] for i in data_sentiment}, output2 + "data_sentiment.txt")
+    writefile({i[0]:i[1] for i in data_keywords}, output2 + "data_keywords.txt")
+    writefile({i[0]:i[1] for i in data_phone}, output2 + "data_phone.txt")
+    writefile({i[0]:i[1] for i in data_internet}, output2 + "data_internet.txt")
+    writefile({i[0]:i[1] for i in data_social}, output2 + "data_social.txt")
+    writefile({i[0]:i[1] for i in data_sentiword}, output2 + "data_sentiword.txt")
 
 
 
