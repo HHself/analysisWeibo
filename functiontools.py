3c9b553aed773f4a7625a7510110fc41a38e87db
huhao <hh87know@gmail.com>
2016-01-14 13:32:17 +0800

    Signed-off-by: huhao <hh87know@gmail.com>

--- functiontools.py
+++ functiontools.py
@@ -449,7 +449,7 @@ def getactivity():
         #     data_source[row['activity']] += int(row['num'])
         # except:
         #     print line
-        # break
+        break
     temp_source= sorted(data_source.iteritems(), key = lambda x:x[1], reverse = True)[:50]
     writefile({i[0]:i[1] for i in temp_source},  " activity.txt")
 
