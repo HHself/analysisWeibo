0f51ddc0c857757d80683a6f1bde5ec68f99f955
huhao <hh87know@gmail.com>
2016-01-14 12:59:19 +0800

    Signed-off-by: huhao <hh87know@gmail.com>

--- functiontools.py
+++ functiontools.py
@@ -439,7 +439,7 @@ def getactivity():
 
 
         for nu in d_source.index:
-            print nu,d_source['activity_num'][nu]
+            # print nu,d_source['activity_num'][nu]
             data_source.setdefault(nu, 0)
             data_source[nu] += int(d_source["activity_num"][nu])
         # for index,row in d_source.iterrows():
