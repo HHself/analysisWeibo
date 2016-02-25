8f4ac3b5c76faed4585fc5e4da82eb6f6319c1fb
huhao <hh87know@gmail.com>
2016-02-25 15:03:21 +0800

    Signed-off-by: huhao <hh87know@gmail.com>

--- functiontools.py
+++ functiontools.py
@@ -463,11 +463,11 @@ def getwordnumdistri():
     precol = ["userID", "username", "screenname", "msginfo", "source", "forwardNum", "commentNum", "releasetime", "etuser"]
     data_source = {}  
     num = 0
-    for line in file(output4 + "content.txt"):
+    for line in file(filepath4 + "content.txt"):
         num +=1
         if num%100==0: print num
         try:
-            d = pd.read_csv(output4 + line.replace("\n",""))
+            d = pd.read_csv(filepath4 + line.replace("\n",""))
             d_source = pd.DataFrame(d['msginfo'])
             d_source["word_num"] =  d['msginfo'].apply(lambda x:len(re.sub("\\pP‘’“”","",str(x).decode("utf-8"))))
             print d['msginfo'][1],d_source["word_num"][1]
