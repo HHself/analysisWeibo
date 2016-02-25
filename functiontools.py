40d200d5d7fe454334c20063c8973b42b04c2960
huhao <hh87know@gmail.com>
2016-02-25 15:57:55 +0800

    Signed-off-by: huhao <hh87know@gmail.com>

--- functiontools.py
+++ functiontools.py
@@ -469,7 +469,7 @@ def getwordnumdistri():
         try:
             d = pd.read_csv(filepath4 + line.replace("\n",""))
             d_source = pd.DataFrame(d['msginfo'])
-            d_source["word_num"] =  d['msginfo'].apply(lambda x:len(sub(ur"[^\u4e00-\u9fa5]","",str(x).decode("utf-8"))))
+            d_source["word_num"] =  d['msginfo'].apply(lambda x:len(re.sub(ur"[^\u4e00-\u9fa5]","",str(x).decode("utf-8"))))
             print d['msginfo'][1],d_source["word_num"][1]
             d_source = d_source.groupby('word_num').count()
             for nu in d_source.index: 
