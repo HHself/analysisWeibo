f83bff3f9366c1134692422895306d4074d11c2c
HH <hh87know@gmail.com>
2016-03-04 17:44:21 +0800

    Signed-off-by: HH <hh87know@gmail.com>

--- lstm_rnn.py
+++ lstm_rnn.py
@@ -1,10 +1,10 @@
 import numpy as np
 import scipy as sp
-from math import exp
+import math 
 
-sigmoid = lambda x : 1.0/(1+exp(-x))
+sigmoid = lambda x : 1.0/(1 + math.exp(-x))
 tanh = lambda x : numpy.tanh(x)
-cos = lambda x,y : numpy.cos(x,y)
+cos = lambda x,y : numpy.cos(x, y)
 
 class LSTM_RNN():
 	 """Long short-term memory.
@@ -17,7 +17,7 @@ class LSTM_RNN():
 
     """
 
-	def __init__(self, param, text, textvec):
+	def __init__(self, param, text, textvec, k=3):
 	    self.W1 = np.array(param[0]) # param: W1, W2, W3, W4, Wr1, Wr2, Wr3, Wr4, Wp1, Wp2, Wp3, b1, b2, b3, b4
 	    self.W2 = np.array(param[1])
 	    self.W3 = np.array(param[2])
@@ -36,8 +36,9 @@ class LSTM_RNN():
 	    self.textvec = np.array(textvec)
 	    self.text = text # it's unicode without non-chinese
 	    self.textlen = len(text)
+	    self.k = k # the number of keyword
     
-    def lstmrun(self, flag="last"):
+    def lstmrun(self, flag = "last"):
     	'''
     	    flag means output all vectors or last vector
     	'''
@@ -56,15 +57,42 @@ class LSTM_RNN():
     		ot  = sigmoid(np.dot(self.W1, curvec) + np.dot(self.Wr1, y_before) +np.dot(self.Wp1, ct) + self.b1)
     		yt  = ot * tanh(ct)
     		y_output.append(yt)
-    	if flag == "all":
-    		return y_output
-    	else:
+    	if flag == "last":
     		return y_output[-1]
+    	else:
+    		return y_output
 
     def BPTTtrain(self, ): 
 
     def getkeywordind(self):
-    	y_output = self.lstmrun()
+    	y_output = self.lstmrun(flag = "all")
+    	dis = []
+    	for num in range(textlen-1):
+    		dis.append(cossim(y_output[num], y_output[num+1]))
+    	dis.sort()#bug：当元素相同
+    	keyind = [dis.index(i) for i in dis]
+    	f = 0
+    	while f < textlen:
+    		for j in range(f, textlen):
+
+
+
+
+
+
+
+    def cossim(self, ls1, ls2):
+        if ls1.shape != ls2.shape:
+            print "error ,list not equal"
+            return
+        m1 = 0
+        m2 = 0
+        sum = 0
+        for i in xrange(len(ls1)):
+            m1 += math.pow(ls1[i], 2)
+            m2 += math.pow(ls2[i], 2)
+            sum +=ls1[i] * ls2[i]
+        return sum/(math.sqrt(m1) *math.sqrt(m2))
 
 
 
