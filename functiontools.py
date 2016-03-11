2eeb131d0eef7a67770388614a5c37a56f03fe53
HH <hh87know@gmail.com>
2016-03-11 12:47:01 +0800

    Signed-off-by: HH <hh87know@gmail.com>

--- functiontools.py
+++ functiontools.py
@@ -512,7 +512,7 @@ def gettraindata():
         d = pd.read_csv(filepath4 + line.replace("\n",""))
         d_source = pd.DataFrame(d['msginfo'])
         
-        findactivity = lambda row : row.startswith("#")
+        findactivity = lambda row : str(row).startswith("#")
         d_source = d_source[d_source.apply(findactivity, axis = 1)]
         newFrame = pd.concat([newFrame, d_source])
 
