cfd5be7a9b6c3ea099e8d67c35eb61c8245e919c
HH <hh87know@gmail.com>
2016-03-12 13:05:00 +0800

    Signed-off-by: HH <hh87know@gmail.com>

--- runlstm.py
+++ runlstm.py
@@ -244,9 +244,9 @@ def genworddict(worddict):
     dictdata = [line for line in file(worddict)]
     for line in dictdata:
         da = line.split("\t")
-        word = da[0].decode("utf-8")
+        word = da[0]
         # dictword2vec.setdefault(word, 0)
-        s = "dictword2vec['" + word + "']=" + da[1] 
+        s = "dictword2vec['" + str(word).decode("utf-8")+ "']=" + da[1] 
         exec(s)
     print dictword2vec.keys()
 
