bf8b7ca25dace10ccf326efb361df58310558e22
HH <hh87know@gmail.com>
2016-03-12 12:45:07 +0800

    Signed-off-by: HH <hh87know@gmail.com>

--- runlstm.py
+++ runlstm.py
@@ -198,9 +198,10 @@ def BPTTtrain(parameters):
             data = gettraindata(r, weibo) #data[0]: source, data[1]:posotive, data[2:]:negatives
             for k in range(PN):
                 param.append(parameters[k] + miu * (parameters[k] - param_last[k]))
+            print data
             y_s = getlastoutput(param, data[0])
             y_p = getlastoutput(param, data[1])
-            print y_s
+
             cos_y_sp = cossim(y_s[-1][-1], y_p[-1][-1])
 
             esum = 0
